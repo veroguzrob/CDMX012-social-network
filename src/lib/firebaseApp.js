648b132739e2a8595c3cc8cf69be26fc5f7a319b
veroguzrob72 <vgrdesd@gmail.com>
2022-05-01 16:22:35 -0500

    Se inicia SPA y se dan los estilos a cada vista

---
+++ src/lib/firebaseApp.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWmRVwu31CWbMSpT_4wJYCOZIfO0g5SnQ',
+  authDomain: 'book-lovers-9482b.firebaseapp.com',
+  projectId: 'book-lovers-9482b',
+  storageBucket: 'book-lovers-9482b.appspot.com',
+  messagingSenderId: '801993614327',
+  appId: '1:801993614327:web:30165cbeb97bad16d104f2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
+/* eslint-disable import/no-cycle */
 // Este es el punto de entrada de tu aplicacion
+import { home } from './components/home.js';
+import { register } from './components/register.js';
+import { postPage } from './components/post.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': home,
+  '/register': register,
+  '/post': postPage,
+};
+
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+  rootDiv.appendChild(routes[pathname]());
+};
+
+window.onpopstate = () => {
+  rootDiv.appendChild(routes[window.location.pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+rootDiv.appendChild(component());
